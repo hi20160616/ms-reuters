364c799b42d86580afdfd7968b02ae51bd1c9906
hi20160616 <hi20160616@163.com>
2021-10-15 12:05:16 +0800

    update configs, fix ms panic exit.

--- internal/fetcher/article.go
+++ internal/fetcher/article.go
@@ -32,9 +32,9 @@ type Article struct {
 
 func NewArticle() *Article {
 	return &Article{
-		WebsiteDomain: configs.Data.MS.Domain,
-		WebsiteTitle:  configs.Data.MS.Title,
-		WebsiteId:     fmt.Sprintf("%x", md5.Sum([]byte(configs.Data.MS.Domain))),
+		WebsiteDomain: configs.Data.MS["reuters"].Domain,
+		WebsiteTitle:  configs.Data.MS["reuters"].Title,
+		WebsiteId:     fmt.Sprintf("%x", md5.Sum([]byte(configs.Data.MS["reuters"].Domain))),
 	}
 }
 
@@ -55,8 +55,8 @@ func (a *Article) Get(id string) (*Article, error) {
 			return a, nil
 		}
 	}
-	return nil, fmt.Errorf("[%s] no article with id: %s, url: %s",
-		configs.Data.MS.Title, id, a.U.String())
+	return nil, fmt.Errorf("[%s] no article with id: %s",
+		configs.Data.MS["nytimes"].Title, id)
 }
 
 func (a *Article) Search(keyword ...string) ([]*Article, error) {
@@ -97,9 +97,9 @@ func (u ByUpdateTime) Less(i, j int) bool {
 }
 
 var timeout = func() time.Duration {
-	t, err := time.ParseDuration(configs.Data.MS.Timeout)
+	t, err := time.ParseDuration(configs.Data.MS["reuters"].Timeout)
 	if err != nil {
-		log.Printf("[%s] timeout init error: %v", configs.Data.MS.Title, err)
+		log.Printf("[%s] timeout init error: %v", configs.Data.MS["reuters"].Title, err)
 		return time.Duration(1 * time.Minute)
 	}
 	return t
@@ -146,8 +146,9 @@ func (a *Article) fetchArticle(rawurl string) (*Article, error) {
 
 func (a *Article) fetchTitle() (string, error) {
 	n := exhtml.ElementsByTag(a.doc, "title")
-	if n == nil {
-		return "", fmt.Errorf("[%s] getTitle error, there is no element <title>", configs.Data.MS.Title)
+	if n == nil || len(n) == 0 {
+		return "", fmt.Errorf("[%s] there is no element <title>: %s",
+			configs.Data.MS["reuters"].Title, a.U.String())
 	}
 	title := n[0].FirstChild.Data
 	rp := strings.NewReplacer(" - 路透中文网", "", "| 路透", "")
@@ -158,7 +159,7 @@ func (a *Article) fetchTitle() (string, error) {
 
 func (a *Article) fetchUpdateTime() (*timestamppb.Timestamp, error) {
 	if a.doc == nil {
-		return nil, fmt.Errorf("[%s] doc is nil: %s", configs.Data.MS.Title, a.U.String())
+		return nil, fmt.Errorf("[%s] doc is nil: %s", configs.Data.MS["reuters"].Title, a.U.String())
 	}
 	nodes := exhtml.MetasByProperty(a.doc, "og:article:published_time")
 	cs := []string{}
@@ -170,7 +171,7 @@ func (a *Article) fetchUpdateTime() (*timestamppb.Timestamp, error) {
 		}
 	}
 	if len(cs) <= 0 {
-		return nil, errors.Errorf("[%s] fetchUpdateTime extract nothing: %s", configs.Data.MS.Title, a.U.String())
+		return nil, errors.Errorf("[%s] fetchUpdateTime extract nothing: %s", configs.Data.MS["reuters"].Title, a.U.String())
 	}
 	t, err := time.Parse(time.RFC3339, cs[0])
 	if err != nil {
@@ -186,7 +187,7 @@ func shanghai(t time.Time) time.Time {
 
 func (a *Article) fetchContent() (string, error) {
 	if a.doc == nil {
-		return "", errors.Errorf("[%s] fetchContent: doc is nil: %s", configs.Data.MS.Title, a.U.String())
+		return "", errors.Errorf("[%s] fetchContent: doc is nil: %s", configs.Data.MS["reuters"].Title, a.U.String())
 	}
 	body := ""
 	// Fetch content nodes
